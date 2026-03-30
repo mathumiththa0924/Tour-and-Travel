d0f390cacaa5ffb4cb61899ef3f0e31bca526ba6
mathumiththa0924 <mathumiththa0924@gmail.com>
2026-03-30 12:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const observer=new IntersectionObserverObsever((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+            
+});
+ 
+const hiddenElements=document.querySelectorAll('.hidden');
+hiddenElements.foeEach((el) => observer.observe(el));
